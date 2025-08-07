78da58e9068f61925a54de56855fbd34696adcc5
sergiylaslov472-ship-it <sergiylaslov472@gmail.com>
2025-08-07 18:08:41 +0300

    тест  коміт

--- unit_1/src/App.js
+++ unit_1/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+let text = 'test variable';
 
 function App() {
+  let text2 = '222'
   return (
       <>
   <div className='container'>
  <h1>app_1</h1>
+ <img src="/images/react.png" alt="test" />
+ <ul>
+  <li>Hello</li>
+  <li>{text+text2}</li>
+ </ul>
  </div>
   </>
     );
